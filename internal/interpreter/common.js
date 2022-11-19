97827b6089881fbbf836adbde57be68bad18eb7d
Paramtamtam <7326800+tarampampam@users.noreply.github.com>
2022-11-19 17:40:12 +0400

    wip: runtime

--- internal/interpreter/common.js
+++ internal/interpreter/common.js
@@ -2,11 +2,14 @@ console.log('test', 123, {}, [], [1, 666, 3], undefined, null)
 
 const a = 1
 
-// const resp = await fetch('https://httpbin.org/get')
-// console.log(resp)
+const resp = await(fetch('https://httpbin.org/get'))
+
+console.log(resp)
 
 // console.log(new Promise(resolve => {}))
 
-for (const property in process.env) {
-  console.log(`${property}: ${process.env[property]}`)
-}
+// for (const property in process.env) {
+//   console.log(`${property}: ${process.env[property]}`)
+// }
+
+
