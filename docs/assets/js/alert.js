72d80ac478b6c0118cb0da743795c7140391f11e
Paramtamtam <7326800+tarampampam@users.noreply.github.com>
2022-11-26 13:05:21 +0400

    hugo template deployed

---
+++ docs/assets/js/alert.js
@@ -0,0 +1,18 @@
+const announcement = document.getElementById('announcement');
+
+if (announcement !== null) {
+  let id = announcement.dataset.id;
+
+  Object.keys(localStorage).forEach(function(key) {
+    if (/^global-alert-/.test(key)) {
+      if (key !== id ) {
+        localStorage.removeItem(key);
+        document.documentElement.removeAttribute('data-global-alert');
+      }
+    }
+  });
+
+  announcement.addEventListener('closed.bs.alert', () => {
+    localStorage.setItem(id, 'closed');
+  });
+}
